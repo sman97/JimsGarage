e8fc0426e4ef0fe4c50cb58007d15e9a4629bbb0
Joel DeLany <joel.delany@gmail.com>
2024-09-11 10:46:58 -0700

    Update k3s.sh

--- Kubernetes/K3S-Deploy/k3s.sh
+++ Kubernetes/K3S-Deploy/k3s.sh
@@ -55,7 +55,7 @@ all=($master1 $master2 $master3 $worker1 $worker2)
 allnomaster1=($master2 $master3 $worker1 $worker2)
 
 #Loadbalancer IP range
-lbrange=192.168.3.60-192.168.3.80
+lbrange=192.168.3.60-192.168.3.90
 
 #ssh certificate name variable
 certName=id_rsa
